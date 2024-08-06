4ea057ba5705bf0ed1e70303e9533bb1549514e7
Ping <piyatanalj@gmail.com>
2024-08-06 23:13:30 +0700

    add lint to json file

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -3,12 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// const db = mysql.createConnection({
+//     host: process.env.MYSQL_HOSTNAME,
+//     user: process.env.MYSQL_USERNAME,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DB,
+//     port: process.env.MYSQL_PORT,
+// });
+
 const db = mysql.createConnection({
-    host: process.env.MYSQL_HOSTNAME,
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB,
-    port: process.env.MYSQL_PORT,
+    host: functions.config().db.host,
+    user: functions.config().db.user,
+    password: functions.config().db.password,
+    database: functions.config().db.name,
+    port: functions.config().db.port || 3306, // Default to 3306 if not set
 });
 
 //verify connection between backend server and database
