a6e37c543d512bc7bc8174a4f925f3d1ab5f3d31
Ping <piyatanalj@gmail.com>
2024-08-07 17:47:40 +0700

    use env var for db connection

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+import {onRequest} from "firebase-functions/v2/https";
+import logger from "firebase-functions/logger";
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -16,4 +16,4 @@ const logger = require("firebase-functions/logger");
 // exports.helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
-// });
+// }); 
